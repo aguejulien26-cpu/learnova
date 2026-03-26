0cdb5469bde5de2a8fe619f35f63a3d74d685e97
aguejulien26-cpu <aguejulien26@gmail.com>
2026-03-26 02:13:48 +0100

    Correction config Supabase et erreurs Git

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl ="https://jbshkbqchrehfhoodtuj.supabase.co" // Remplacez par l'URL de votre Supabase
-const supabaseAnonKey = "sb_publishable_iBypuqRKhVqZUvNBmATW-w_I1LJHLRs" // Remplacez par votre clé API publique
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
